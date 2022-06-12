e231e811e25101d835dcf2954bfc3db3ff113cce
QuynhNga4200 <20211TT4200@mail.tdc.edu.vn>
2022-06-12 19:46:43 +0700

    update

--- covid/src/apis/index.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const getCountries = () => axios.get('https://api.covid19api.com/countries')
-
-//  Vì người dùng sẽ chọn xem theo quốc gia nên k đc hashcode nên truyền cho 1 biến country
-export const getReporByCountry = (country) => axios.get(`https://api.covid19api.com/dayone/country/${country}`);
